8f2d1865ed9515a4efac3e50472f7fd96a421f92
Kriish Patel <kriishpatel27@gmail.com>
2025-06-08 13:51:05 -0500

    Update styles and make site mobile friendly

--- src/About.jsx
+++ src/About.jsx
@@ -8,8 +8,8 @@ function About({ heading, title, bio, location, email}) {
         <motion.div
             initial={{ opacity: 0 }}
             whileInView={{ opacity: 1 }}
-            transition={{ duration: 0.8, ease:  [0.42, 0, 1, 1] }}
-            viewport={{ once: false, amount: 0.6 }}>
+            viewport={{ once: false }} // lower the trigger threshold
+            transition={{ duration: 0.8, ease: [0.42, 0, 1, 1] }}>
             <motion.div className='description-container'>
                     <h1 className='heading'>{heading}</h1>
                     <h2 className='sub-heading'>{title}</h2>
@@ -17,9 +17,9 @@ function About({ heading, title, bio, location, email}) {
             </motion.div>
                 
             <motion.div className='pic-container'>
-                <img className='dog-pic' src='dog.jpg'></img>
-                <img className='lego-pic' src='lego.jpg'></img>
-                <img className='hiking-pic' src='hiking.jpg'></img>
+                <img className='dog-pic' src={`${import.meta.env.BASE_URL}dog.jpg`} />
+                <img className='lego-pic' src={`${import.meta.env.BASE_URL}lego.jpg`} />
+                <img className='hiking-pic' src={`${import.meta.env.BASE_URL}hiking.jpg`} />
                 <p className='location'>Location: {location}</p>
                 <p className='email'>Email: {email}</p>
             </motion.div>
